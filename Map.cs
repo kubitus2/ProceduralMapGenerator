52d44c767953b490360a3296e380bc37a74b7ee0
Jakub Osiński <jakubosinski2@gmail.com>
2021-08-05 21:48:28 +0200

    Add project files.

---
+++ Map.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProceduralMapGenerator
+{
+    struct Vector2DInt
+    {
+        int x;
+        int y;
+
+        public Vector2DInt(int X, int Y)
+        {
+            x = X;
+            y = Y;
+        }
+    }
+
+    class Map
+    {
+        Random psrg = new Random();
+
+        private int _mapWidth;
+        private int _mapHeight;
+
+        private int _fillDensity;
+
+        public Map (int mapW, int mapH, int d)
+        {
+            _mapWidth = mapW;
+            _mapHeight = mapH;
+            _fillDensity = d;
+        }
+    }
+}
