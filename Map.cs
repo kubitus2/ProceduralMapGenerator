785025faaf4b5f9311f72361a716e2e4aa4323d1
Jakub Osiński <jakubosinski2@gmail.com>
2021-08-06 22:36:32 +0200

    Created file save system.

---
+++ Map.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProceduralMapGenerator
+{
+
+
+    class Map
+    {
+        const int NUMBER_OF_ITERATIONS = 3;
+
+        readonly Random psrg = new Random();
+
+        private int[,] _map;
+
+        private int _mapWidth;
+        private int _mapHeight;
+
+        private int _fillDensity;
+
+        public Map (int mapW, int mapH, int d)
+        {
+            _mapWidth = mapW;
+            _mapHeight = mapH;
+            _fillDensity = d;
+            _map = new int[mapW, mapH];
+        }
+
+        private int RandomBlock()
+        { 
+
+            int rand = psrg.Next(0, 100);
+            return rand >= _fillDensity ? 0 : 1;
+        }
+        private void RandomFill()
+        {
+            for (int x = 0; x < _mapWidth; x++)
+            {
+                for (int y = 0; y < _mapHeight; y++)
+                {
+                    //ensure that the edge is wall
+                    if (x == 0 || y == 0 || x == _mapWidth - 1 || y == _mapHeight - 1)
+                    {
+                        _map[x, y] = 1;
+                    }
+                    else
+                    {
+                        _map[x, y] = RandomBlock();
+                    }
+                }
+            }
+        }
+
+        private bool IsWall(int x, int y)
+        {
+            return _map[x, y] == 1;
+        }
+
+        //Count number of walls in adjacent cells.
+        private int CountAdjacentWalls(int x, int y)
+        {
+            int wallCount = 0;
+
+            for (int i = x - 1; i <= x + 1; i++)
+            {
+                for (int j = y - 1; j <= y + 1; j++)
+                {
+                    if (!(i == x && j == y))
+                    {
+                        if (IsWall(i, j))
+                            wallCount++;
+                    }
+                }
+            }
+
+            return wallCount;
+        }
+
+        //Place wall logic.
+        private int PlaceWall(int x, int y)
+        {
+            int adjWalls = CountAdjacentWalls(x, y);
+
+            if (_map[x, y] == 1)
+            {
+                if (adjWalls >= 4)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else if (_map[x, y] == 0)
+            {
+                if (adjWalls >= 5)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                return _map[x, y];
+            }
+        }
+        //
+        //Carve caves by cellural automata algorithm.
+        private void CaveMap(int numberOfIterations)
+        {
+            int[,] temp = new int[_mapWidth, _mapHeight];
+            temp = _map;
+
+            for (int i = 0; i < numberOfIterations; i++)
+            {
+                for (int x = 1; x < _mapWidth - 1; x++)
+                {
+                    for (int y = 1; y < _mapHeight - 1; y++)
+                    {
+                        temp[x, y] = PlaceWall(x, y);
+                    }
+                }
+            }
+
+            _map = temp;
+        }
+        private void WriteAt(int x, int y, string s)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(s);
+        }
+
+        private string Symbol(int x, int y)
+        {
+            return _map[x, y] == 1 ? "#" : " ";
+        }
+
+        public void Init()
+        {
+            RandomFill();
+            CaveMap(NUMBER_OF_ITERATIONS);
+        }
+        public void ChangeDensity(int sign)
+        {
+            _fillDensity += 5 * sign;
+
+            if (_fillDensity < 0)
+                _fillDensity = 0;
+            else if (_fillDensity > 100)
+                _fillDensity = 100;
+        }
+        public void Draw()
+        {
+            for (int x = 0; x < _mapWidth; x++)
+            {
+                for (int y = 0; y < _mapHeight; y++)
+                {
+                    WriteAt(x, y, Symbol(x, y));
+                }
+            }
+        }
+
+        public void Reset()
+        {
+            Init();
+        }
+
+        public int Density
+        {
+            get
+            {
+                return _fillDensity;
+            }
+        }
+
+        public string MapToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            string nextChar;
+
+            for(int rows = 0; rows < _mapHeight; rows++)
+            {
+                for(int cols = 0; cols < _mapWidth; cols++)
+                {
+                    nextChar = Symbol(cols, rows);
+                    sb.Append(nextChar);
+                }
+                sb.Append("\n");
+            }
+
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ProceduralMapGenerator
+{
+    class Program
+    {
+        const int MAP_WIDTH = 60;
+        const int MAP_HEIGHT = 30;
+        const int INITIAL_DENSITY = 45;
+
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Console.Clear();
+
+            MapBrowser();
+
+        }
+
+        static void MapBrowser()
+        {
+
+            Map map = new Map(MAP_WIDTH, MAP_HEIGHT, INITIAL_DENSITY);
+            map.Init();
+
+            while (true)
+            {
+                ConsoleKeyInfo input = Console.ReadKey(true);
+
+                switch (input.Key)
+                {
+                    case ConsoleKey.Escape:
+                        Environment.Exit(0);
+                        break;
+                    case ConsoleKey.N:
+                        map.Reset();
+                        break;
+                    case ConsoleKey.OemPlus:
+                        map.ChangeDensity(1);
+                        break;
+                    case ConsoleKey.OemMinus:
+                        map.ChangeDensity(-1);
+                        break;
+                    case ConsoleKey.S:
+                        SaveMap(map.MapToString());
+                        break;
+                }
+                map.Draw();
+                DisplayMenu(map.Density);
+
+            }
+
+        }
+
+        static void DisplayMenu(int d)
+        {
+            Console.SetCursorPosition(MAP_WIDTH + 3, 0);
+            Console.Write("Controls: ");
+            Console.SetCursorPosition(MAP_WIDTH + 3, 1);
+            Console.Write(" [N] - next map.");
+            Console.SetCursorPosition(MAP_WIDTH + 3, 2);
+            Console.Write(" [+/-] - change fill density.");
+            Console.SetCursorPosition(MAP_WIDTH + 3, 4);
+            Console.Write(" [S] - save map to file. ");
+            Console.SetCursorPosition(MAP_WIDTH + 3, 5);
+            Console.Write(" [Esc] - exit app. ");
+            Console.SetCursorPosition(MAP_WIDTH + 3, 3);
+            Console.Write("Fill density: {0}     ", d);
+        }
+
+        static void SaveMap(string map)
+        {
+            string fileName = string.Empty;
+
+            Console.Clear();
+            Console.WriteLine("Give file name: \n");
+            fileName = Console.ReadLine();
+
+            fileName += ".txt";
+
+            WriteToFile(fileName, map);
+
+        }
+
+        static void WriteToFile(string fileName, string content)
+        {
+            try
+            {
+                if (File.Exists(fileName))
+                {
+                    throw new ArgumentException("File with this name already exists!");
+                }
+                
+                using (StreamWriter sw = File.CreateText(fileName))
+                {
+                    sw.WriteLine("New map saved at {0}", DateTime.Now.ToString());
+                    sw.WriteLine(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Clear();
+                Console.WriteLine(ex.ToString());
+                Console.ReadLine();
+            }
+        }
+
+
+    }
+}
